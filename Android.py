405722be8d401b42561d8a9ae775db4078cc3ece
Dmitriy Puzak <dmitriy.puzak@moviemethod.com>
2021-03-25 19:50:47 +0200

    Added replaces for android strings.

--- Android.py
+++ Android.py
@@ -3,9 +3,10 @@ import os
 ANDROID_LOCALIZATION_DIR_NAME_EN = "android/res/values"
 ANDROID_LOCALIZATION_DIR_NAME_MASK = "android/res/values-{0}"
 ANDROID_LOCALIZATION_FILE_NAME = "strings.xml"
-ANDROID_PLURAL_LOCALIZATION_FILE_NAME = "plural.xml"
+ANDROID_PLURAL_LOCALIZATION_FILE_NAME = "plurals.xml"
 
-ANDROID_LOCALIZATION_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n"
+ANDROID_LOCALIZATION_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources " \
+                              "xmlns:tools=\"http://schemas.android.com/tools\" tools:ignore=\"UnusedQuantity\">\n "
 ANDROID_LOCALIZATION_FORMAT = "\t<string name=\"{0}\">{1}</string>\n"
 ANDROID_PLURAL_LOCALIZATION_FORMAT = "\t<plurals name=\"{0}\">\n"
 ANDROID_PLURAL_LOCALIZATION_END_FORMAT = "\t</plurals>\n"
@@ -53,11 +54,14 @@ def androidLocalization(translations, languageId):
     contents = ANDROID_LOCALIZATION_HEADER
     for tr in translations:
         # Ignore empty keys.
-        if tr.androidKey is None:
+        if tr.androidKey is None or not tr.translations[languageId]:
             continue
         contents += ANDROID_LOCALIZATION_FORMAT.format(tr.androidKey, tr
                                                        .translations[languageId]
                                                        .replace('%i', '%d')
+                                                       .replace('%@', '%s')
+                                                       .replace("\'", "\\'")
+                                                       .replace('&', '&amp;')
                                                        .encode('utf8'))
     contents += ANDROID_LOCALIZATION_FOOTER
     return contents
@@ -75,7 +79,10 @@ def androidPluralLocalization(pluralTranslations, languageId):
             translation = value[languageId]
             if not translation:
                 continue
-            contents += ANDROID_PLURAL_ITEM_LOCALIZATION_END_FORMAT.format(key, translation.replace('%i', '%d'))
+            contents += ANDROID_PLURAL_ITEM_LOCALIZATION_END_FORMAT.format(key, translation
+                                                                           .replace('%i', '%d')
+                                                                           .replace('&', '&amp;')
+                                                                           .replace("\'", "\\'"))
         contents += ANDROID_PLURAL_LOCALIZATION_END_FORMAT
     contents += ANDROID_LOCALIZATION_FOOTER
     return contents
